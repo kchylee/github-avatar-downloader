ba437db44dea91ce86b4ce2bea99b08c9c2568c7
Ken <kchylee@gmail.com>
2017-04-10 20:40:41 +0000

    Added requestURL and tested to return JSON

--- download_avatar.js
+++ download_avatar.js
@@ -2,6 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
+var GITHUB_USER = "kchylee";
+var GITHUB_TOKEN = "a87af952cbc3561942e9308e79476650d1d163fe";
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + 'jquery' + '/' + 'jquery' + '/contributors';
+
+
+console.log(requestURL);
 function getRepoContributors (repoOwner, repoName, cb){
 
 }
