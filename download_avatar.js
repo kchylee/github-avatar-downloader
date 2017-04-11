0854f880e05e202b33b2330e0a7094e34cdd800d
Ken <kchylee@gmail.com>
2017-04-11 01:14:27 +0000

    Added error responses

--- download_avatar.js
+++ download_avatar.js
@@ -1,7 +1,11 @@
 var request = require('request');
 var fs = require('fs');
 
-console.log('Welcome to the Github Avatar Downloader!');
+var args = process.argv.slice(2);
+if (args == ""){
+  console.log("Input required");
+  return;
+}
 
 var GITHUB_USER = "kchylee";
 var GITHUB_TOKEN = "a87af952cbc3561942e9308e79476650d1d163fe";
@@ -12,26 +16,30 @@ function getRepoContributors (repoOwner, repoName, cb){
     url: requestURL,
     method: "GET",
     headers: {
-      'User-Agent': 'Github Avatar Downloader - Student Project'
+      'User-Agent': 'skratchtherecord'
     }
   }
   request(options, function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body);
       cb(error, info);
+    }else{
+      console.log("Error " + response.statusCode + " : " + error);
     }
-  });
+  })
 }
-
-getRepoContributors('jquery', 'jquery', function(err, result){
-  for (index in result){
-    console.log(result[index].avatar_url);
-    // downloadImageByURL("https://avatars3.githubusercontent.com/u/1615?v=3", "avatar/jeresig.jpg");
-    downloadImageByURL(result[index].avatar_url, "avatars/" + result[index].login + ".jpg");
-  }
-});
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .pipe(fs.createWriteStream(filePath));
 }
+
+getRepoContributors(args[0], args[1], function(err, result){
+  if(!err){
+    for (index in result){
+    downloadImageByURL(result[index].avatar_url, "avatars/" + result[index].login + ".jpg");
+    }
+  }
+  console.log("Download complete");
+});
+
